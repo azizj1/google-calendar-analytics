a1027ef2d59e27f58e499c0d6a7625d2ee791844
Aziz <azizjaved0@gmail.com>
2020-02-03 20:04:06 -0600

    Update swagger json for auth

--- src/routes/calendars/auth.ts
+++ src/routes/calendars/auth.ts
@@ -5,7 +5,7 @@ const routes = Router();
 routes.use('/', async (req, res: Response, next: NextFunction) => {
     const accessKey = req && req.query && req.query.accessKey;
     if (accessKey !== authJson.password)
-        return res.status(401).end('Unauthorized.');
+        return res.status(401).json('Unauthorized');
     next();
 });
 
